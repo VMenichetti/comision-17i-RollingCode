95d9ee76eec0fcdbf0fa37d0464d178dbbf7a8e0
VERO MENICHETTI <ingvmenichetti@gmail.com>
2022-11-03 20:32:39 -0300

    MODULO3

--- JAVASCRIPT/js/app.js
+++ JAVASCRIPT/js/app.js
@@ -1,3 +1,15 @@
+// OBJETOS
+
+let alumno = {
+    nombre: 'Josefina Alvarez',
+    edad: 38,
+    DNI: '99148728',
+    curso: 'Fullstack',
+};
+
+//Obtener valores
+console.log{alumno.nombre};
+console.log [alumno['curso']]  ;
 
 
 
